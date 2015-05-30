f676625296d3e40a017ba6f42372b0bea758b604
Tony Tin Nguyen <nguyenquangtin@gmail.com>
2015-05-31 00:46:05 +0700

    - Add product all page

---
+++ html/product-all.php
@@ -0,0 +1,291 @@
+<?php
+require  'top.php';
+?>
+<div class="wrapper">
+  <?php
+  require  'header.php';
+  ?>
+  <div class="content about-content">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-lg-12">
+          <div class="text-center">
+            <p class="category-description">
+              Get your fix of organic, cold-pressed goodness. Satisfy your inner rabbit’s craving. Have a
+              mmmmmarvelicious cold-pressed juice, made from fresh, organic ingredients. One juice leads
+              to another. And before you know it, you’re on your way to a healthy lifestyle. Yippee!
+            </p>
+            <div class="category-products">
+
+              <ul class="products-grid">
+
+                <li class="item col-lg-4">
+
+                  <a href="">
+                    <img src="images/products/akalizer.png" height="310" width="310" alt="Alkalizer Bunny">
+                  </a>
+
+                  <h2 class="product-name">
+                    <a href="#" title="Alkalizer Bunny">Alkalizer Bunny</a>
+                  </h2>
+
+                  <ul class="benefit">
+                    <li class="detoxflush" style="background-image: url('images/products/benefits/detoxflush.png');">DETOX &amp; FLUSH</li>
+                    <li class="energybooster" style="background-image: url('images/products/benefits/energybooster.png');">ENERGY BOOSTER</li>
+                  </ul>
+
+                  <div class="clearer"></div>
+
+                  <div class="elements col-lg-6 col-lg-offset-3">
+                    CELERY, CUCUMBER, GREEN APPLE, KALE, LEMON, PARSLEY, ROMAINE, SPINACH
+                  </div>
+
+                  <div style="color: rgb(7, 102, 51);" class="benefit-label col-lg-12">
+                    ENERGY BOOSTER
+                  </div>
+                </li>
+                <!-- item -->
+
+                <li class="item col-lg-4">
+
+                  <a href="">
+                    <img src="images/products/nibble.png" height="310" width="310" alt="Alkalizer Bunny">
+                  </a>
+
+                  <h2 class="product-name">
+                    <a href="#" title="Alkalizer Bunny">Apple Pineapple Nibble</a>
+                  </h2>
+
+                  <ul class="benefit">
+                    <li class="energybooster" style="background-image: url('images/products/benefits/energybooster.png');">ENERGY BOOSTER</li>
+                  </ul>
+
+                  <div class="clearer"></div>
+
+                  <div class="elements col-lg-6 col-lg-offset-3">
+                    CUCUMBER, GREEN APPLE, MINT, PINEAPPLE
+                  </div>
+
+                  <div style="color: rgb(7, 102, 51);" class="benefit-label col-lg-12">
+                    ENERGY BOOSTER
+                  </div>
+                </li>
+                <!-- item -->
+
+                <li class="item col-lg-4">
+
+                  <a href="">
+                    <img src="images/products/bunnt-coco.png" height="310" width="310" alt="Alkalizer Bunny">
+                  </a>
+
+                  <h2 class="product-name">
+                    <a href="#" title="Alkalizer Bunny">Bunny's Coco Fix</a>
+                  </h2>
+
+                  <ul class="benefit">
+                    <li class="antioxidantshot" style="background-image: url('images/products/benefits/antioxidantshot.png')">ANTIOXIDANT SHOT</li>
+                    <li class="hearthealth" style="background-image: url('images/products/benefits/hearthealth.png')">HEART HEALTH</li>
+                    <li class="musclepower" style="background-image: url('images/products/benefits/musclepower.png')">MUSCLE POWER</li>
+
+                  </ul>
+
+                  <div class="clearer"></div>
+
+                  <div class="elements col-lg-6 col-lg-offset-3">
+                    CELERY, CUCUMBER, GREEN APPLE, KALE, LEMON, PARSLEY, ROMAINE, SPINACH
+                  </div>
+
+                  <div style="color: rgb(7, 102, 51);" class="benefit-label col-lg-12">
+                    ENERGY BOOSTER
+                  </div>
+                </li>
+                <!-- item -->
+              </ul>
+              <!-- products-grid -->
+
+
+              <ul class="products-grid">
+
+                <li class="item col-lg-4">
+
+                  <a href="">
+                    <img src="images/products/akalizer.png" height="310" width="310" alt="Alkalizer Bunny">
+                  </a>
+
+                  <h2 class="product-name">
+                    <a href="#" title="Alkalizer Bunny">Alkalizer Bunny</a>
+                  </h2>
+
+                  <ul class="benefit">
+                    <li class="detoxflush" style="background-image: url('images/products/benefits/detoxflush.png');">DETOX &amp; FLUSH</li>
+                    <li class="energybooster" style="background-image: url('images/products/benefits/energybooster.png');">ENERGY BOOSTER</li>
+                  </ul>
+
+                  <div class="clearer"></div>
+
+                  <div class="elements col-lg-6 col-lg-offset-3">
+                    CELERY, CUCUMBER, GREEN APPLE, KALE, LEMON, PARSLEY, ROMAINE, SPINACH
+                  </div>
+
+                  <div style="color: rgb(7, 102, 51);" class="benefit-label col-lg-12">
+                    ENERGY BOOSTER
+                  </div>
+                </li>
+                <!-- item -->
+
+                <li class="item col-lg-4">
+
+                  <a href="">
+                    <img src="images/products/nibble.png" height="310" width="310" alt="Alkalizer Bunny">
+                  </a>
+
+                  <h2 class="product-name">
+                    <a href="#" title="Alkalizer Bunny">Apple Pineapple Nibble</a>
+                  </h2>
+
+                  <ul class="benefit">
+                    <li class="energybooster" style="background-image: url('images/products/benefits/energybooster.png');">ENERGY BOOSTER</li>
+                  </ul>
+
+                  <div class="clearer"></div>
+
+                  <div class="elements col-lg-6 col-lg-offset-3">
+                    CUCUMBER, GREEN APPLE, MINT, PINEAPPLE
+                  </div>
+
+                  <div style="color: rgb(7, 102, 51);" class="benefit-label col-lg-12">
+                    ENERGY BOOSTER
+                  </div>
+                </li>
+                <!-- item -->
+
+                <li class="item col-lg-4">
+
+                  <a href="">
+                    <img src="images/products/bunnt-coco.png" height="310" width="310" alt="Alkalizer Bunny">
+                  </a>
+
+                  <h2 class="product-name">
+                    <a href="#" title="Alkalizer Bunny">Bunny's Coco Fix</a>
+                  </h2>
+
+                  <ul class="benefit">
+                    <li class="antioxidantshot" style="background-image: url('images/products/benefits/antioxidantshot.png')">ANTIOXIDANT SHOT</li>
+                    <li class="hearthealth" style="background-image: url('images/products/benefits/hearthealth.png')">HEART HEALTH</li>
+                    <li class="musclepower" style="background-image: url('images/products/benefits/musclepower.png')">MUSCLE POWER</li>
+
+                  </ul>
+
+                  <div class="clearer"></div>
+
+                  <div class="elements col-lg-6 col-lg-offset-3">
+                    CELERY, CUCUMBER, GREEN APPLE, KALE, LEMON, PARSLEY, ROMAINE, SPINACH
+                  </div>
+
+                  <div style="color: rgb(7, 102, 51);" class="benefit-label col-lg-12">
+                    ENERGY BOOSTER
+                  </div>
+                </li>
+                <!-- item -->
+              </ul>
+              <!-- products-grid -->
+
+              <ul class="products-grid">
+
+                <li class="item col-lg-4">
+
+                  <a href="">
+                    <img src="images/products/akalizer.png" height="310" width="310" alt="Alkalizer Bunny">
+                  </a>
+
+                  <h2 class="product-name">
+                    <a href="#" title="Alkalizer Bunny">Alkalizer Bunny</a>
+                  </h2>
+
+                  <ul class="benefit">
+                    <li class="detoxflush" style="background-image: url('images/products/benefits/detoxflush.png');">DETOX &amp; FLUSH</li>
+                    <li class="energybooster" style="background-image: url('images/products/benefits/energybooster.png');">ENERGY BOOSTER</li>
+                  </ul>
+
+                  <div class="clearer"></div>
+
+                  <div class="elements col-lg-6 col-lg-offset-3">
+                    CELERY, CUCUMBER, GREEN APPLE, KALE, LEMON, PARSLEY, ROMAINE, SPINACH
+                  </div>
+
+                  <div style="color: rgb(7, 102, 51);" class="benefit-label col-lg-12">
+                    ENERGY BOOSTER
+                  </div>
+                </li>
+                <!-- item -->
+
+                <li class="item col-lg-4">
+
+                  <a href="">
+                    <img src="images/products/nibble.png" height="310" width="310" alt="Alkalizer Bunny">
+                  </a>
+
+                  <h2 class="product-name">
+                    <a href="#" title="Alkalizer Bunny">Apple Pineapple Nibble</a>
+                  </h2>
+
+                  <ul class="benefit">
+                    <li class="energybooster" style="background-image: url('images/products/benefits/energybooster.png');">ENERGY BOOSTER</li>
+                  </ul>
+
+                  <div class="clearer"></div>
+
+                  <div class="elements col-lg-6 col-lg-offset-3">
+                    CUCUMBER, GREEN APPLE, MINT, PINEAPPLE
+                  </div>
+
+                  <div style="color: rgb(7, 102, 51);" class="benefit-label col-lg-12">
+                    ENERGY BOOSTER
+                  </div>
+                </li>
+                <!-- item -->
+
+                <li class="item col-lg-4">
+
+                  <a href="">
+                    <img src="images/products/bunnt-coco.png" height="310" width="310" alt="Alkalizer Bunny">
+                  </a>
+
+                  <h2 class="product-name">
+                    <a href="#" title="Alkalizer Bunny">Bunny's Coco Fix</a>
+                  </h2>
+
+                  <ul class="benefit">
+                    <li class="antioxidantshot" style="background-image: url('images/products/benefits/antioxidantshot.png')">ANTIOXIDANT SHOT</li>
+                    <li class="hearthealth" style="background-image: url('images/products/benefits/hearthealth.png')">HEART HEALTH</li>
+                    <li class="musclepower" style="background-image: url('images/products/benefits/musclepower.png')">MUSCLE POWER</li>
+
+                  </ul>
+
+                  <div class="clearer"></div>
+
+                  <div class="elements col-lg-6 col-lg-offset-3">
+                    CELERY, CUCUMBER, GREEN APPLE, KALE, LEMON, PARSLEY, ROMAINE, SPINACH
+                  </div>
+
+                  <div style="color: rgb(7, 102, 51);" class="benefit-label col-lg-12">
+                    ENERGY BOOSTER
+                  </div>
+                </li>
+                <!-- item -->
+              </ul>
+              <!-- products-grid -->
+
+            </div>
+            <!-- category-products -->
+
+          </div>
+        </div>
+      </div>
+      </div> <!-- /container -->
+    </div>
+    </div><!-- /wrapper -->
+    <?php
+    require  'footer.php';
+    require  'bottom.php';
+    ?>
