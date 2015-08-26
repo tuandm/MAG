a08acd329c0e8e1c8b3e4ebd12cc42d8ce023168
pnsreirc <phatnt2721989@gmail.com>
2015-08-27 03:04:30 +0700

    - Add index, product-all, product-detail for demo page.

--- src/js/phatnguyen/main.js
+++ src/js/phatnguyen/main.js
@@ -1,7 +1,7 @@
 
 // Can also be used with $(document).ready()
-$(window).load(function() {
-  $('.flexslider').flexslider({
+jQuery(window).load(function() {
+  jQuery('.flexslider').flexslider({
     animation: "slide"
   });
 });
