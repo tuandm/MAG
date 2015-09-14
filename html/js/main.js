8c0b182e39237d879c68611740b6d0bbdff274dc
Tony Tin Nguyen <nguyenquangtin@gmail.com>
2015-09-14 09:54:08 +0700

    Update HTML + Random logo + fade effect slide => fix error on slide bleeding + add social networks + add subscribe text input + Index + Product page + Pick a dose + About + Contact

--- html/js/main.js
+++ html/js/main.js
@@ -1,7 +1,24 @@
 
-// Can also be used with $(document).ready()
+
 $(window).load(function() {
+  
+  // Run the slider with fade effect
   $('.flexslider').flexslider({
-    animation: "slide"
+    animation: "fade"
   });
+
+  // random logos 1-3
+  var images = ['logo1.png', 'logo2.png', 'logo3.png'];
+  $('#logo').attr("src", 'images/' + images[Math.floor(Math.random() * images.length)]);
+
+
+  $("li ul li").hide();
+  $("ul.main-nav li").hover(
+      function() {
+          $("ul.main-nav li ul li", this).show();
+      }, function() {
+          $("ul.main-nav li ul li", this).hide();
+      }
+  );
+
 });
